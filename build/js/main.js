e6756c72b4c9c4e6000db8c41f83f41e218d709b
ismailtaher <ismail.taher@ymail.com>
2025-05-02 17:31:29 +0500

    add basic function types

--- build/js/main.js
+++ build/js/main.js
@@ -6,3 +6,29 @@
 let myName;
 //myName = "Mirza"
 let userName;
+//userName = "Abdullah";
+userName = "Ismail";
+//
+// Functions
+//
+// defining output type
+const add = (a, b) => {
+    return a + b;
+};
+// function that doesn't return anything (void)
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg("hello!");
+logMsg(add(2, 3));
+// normal function
+let subtract = function (c, d) {
+    return c - d;
+};
+/* interface mathFunction {
+  (a: number, b: number): number;
+} */
+let multiply = function (c, d) {
+    return c * d;
+};
+logMsg(multiply(2, 2));
--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,42 @@ let myName: "Ismail";
 
 let userName: "Muhammad" | "Ismail" | "Mirza";
 
-userName = "Abdullah";
+//userName = "Abdullah";
 
 userName = "Ismail";
+
+//
+// Functions
+//
+
+// defining output type
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+// function that doesn't return anything (void)
+const logMsg = (message: any): void => {
+  console.log(message);
+};
+
+logMsg("hello!");
+logMsg(add(2, 3));
+
+// normal function
+let subtract = function (c: number, d: number): number {
+  return c - d;
+};
+
+// creating a type for a function
+
+type mathFunction = (a: number, b: number) => number;
+
+/* interface mathFunction {
+  (a: number, b: number): number;
+} */
+
+let multiply: mathFunction = function (c, d) {
+  return c * d;
+};
+
+logMsg(multiply(2, 2));
